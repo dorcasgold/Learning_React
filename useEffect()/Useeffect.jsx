a648c4016b45c39c3b33206b251dfed0475607a3
odetayo dorcas <odetayodorcas123@gmail.com>
2024-03-01 20:36:51 +0100

    Update Useeffect.jsx

--- useEffect()/Useeffect.jsx
+++ useEffect()/Useeffect.jsx
@@ -1,29 +1,20 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function Useeffect() {
-  const [isHungry, setIsHungry] = useState(true);
-  const [breathefire, setBreathefire] = useState(true)
-
-  useEffect(() => {
-    if (isHungry) {
-      console.log("Time to feed Sparky!");
-      setIsHungry(false);
-    }
-  }, [isHungry]);
+function ExampleComponent() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    if (breathefire) {
-      console.log("breathe fire!!!!!");
-      setBreathefire(false)
-    }
-  }, [breathefire])
+    document.title = `You clicked ${count} times`;
+  }, []);
 
   return (
-    <>
-      <button onClick={() => setIsHungry(true)}>Make Sparky hungry</button>
-      <button onClick={() => setBreathefire(true)}>Make Sparky breathe fire</button>
-    </>
-  )
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default Useeffect
+export default ExampleComponent;
